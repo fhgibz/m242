b374c9b4b2b7244e647b70ec7da3a56473de79a0
Florian <fhaenny@online.gibz.ch>
2020-05-09 00:11:31 +0100

    programm konnte auf bördli geladen werden, aber Hello World erscheint nicht

---
+++ HelloWorld/Build/HelloWorld.d
@@ -0,0 +1,57 @@
+C:\OrdnerFuerSachenWoImSharedFolderNichtGehen\laich\M242\m242\HelloWorld\Build\HelloWorld.d \
+ C:\OrdnerFuerSachenWoImSharedFolderNichtGehen\laich\M242\m242\HelloWorld\Build\HelloWorld.o: \
+ C:\OrdnerFuerSachenWoImSharedFolderNichtGehen\laich\M242\m242\HelloWorld\Build\HelloWorld.c \
+ c:\ordnerfuersachenwoimsharedfoldernichtgehen\laich\m242\m242\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\io.h \
+ c:\ordnerfuersachenwoimsharedfoldernichtgehen\laich\m242\m242\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\sfr_defs.h \
+ c:\ordnerfuersachenwoimsharedfoldernichtgehen\laich\m242\m242\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\inttypes.h \
+ c:\ordnerfuersachenwoimsharedfoldernichtgehen\laich\m242\m242\toolchain\avr8-gnu-toolchain-win32_x86\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\ordnerfuersachenwoimsharedfoldernichtgehen\laich\m242\m242\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\stdint.h \
+ c:\ordnerfuersachenwoimsharedfoldernichtgehen\laich\m242\m242\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\iom328p.h \
+ c:\ordnerfuersachenwoimsharedfoldernichtgehen\laich\m242\m242\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\portpins.h \
+ c:\ordnerfuersachenwoimsharedfoldernichtgehen\laich\m242\m242\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\common.h \
+ c:\ordnerfuersachenwoimsharedfoldernichtgehen\laich\m242\m242\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\version.h \
+ c:\ordnerfuersachenwoimsharedfoldernichtgehen\laich\m242\m242\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\fuse.h \
+ c:\ordnerfuersachenwoimsharedfoldernichtgehen\laich\m242\m242\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\lock.h \
+ C:\OrdnerFuerSachenWoImSharedFolderNichtGehen\laich\M242\m242\ToolChain\AvrLib\include/Atmega328P.h \
+ C:\OrdnerFuerSachenWoImSharedFolderNichtGehen\laich\M242\m242\ToolChain\AvrLib\include/avrlib.h \
+ c:\ordnerfuersachenwoimsharedfoldernichtgehen\laich\m242\m242\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\util\delay.h \
+ c:\ordnerfuersachenwoimsharedfoldernichtgehen\laich\m242\m242\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\util\delay_basic.h \
+ c:\ordnerfuersachenwoimsharedfoldernichtgehen\laich\m242\m242\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\math.h \
+ c:\ordnerfuersachenwoimsharedfoldernichtgehen\laich\m242\m242\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\interrupt.h \
+ c:\ordnerfuersachenwoimsharedfoldernichtgehen\laich\m242\m242\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\io.h
+
+c:\ordnerfuersachenwoimsharedfoldernichtgehen\laich\m242\m242\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\io.h:
+
+c:\ordnerfuersachenwoimsharedfoldernichtgehen\laich\m242\m242\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\sfr_defs.h:
+
+c:\ordnerfuersachenwoimsharedfoldernichtgehen\laich\m242\m242\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\inttypes.h:
+
+c:\ordnerfuersachenwoimsharedfoldernichtgehen\laich\m242\m242\toolchain\avr8-gnu-toolchain-win32_x86\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\ordnerfuersachenwoimsharedfoldernichtgehen\laich\m242\m242\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\stdint.h:
+
+c:\ordnerfuersachenwoimsharedfoldernichtgehen\laich\m242\m242\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\iom328p.h:
+
+c:\ordnerfuersachenwoimsharedfoldernichtgehen\laich\m242\m242\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\portpins.h:
+
+c:\ordnerfuersachenwoimsharedfoldernichtgehen\laich\m242\m242\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\common.h:
+
+c:\ordnerfuersachenwoimsharedfoldernichtgehen\laich\m242\m242\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\version.h:
+
+c:\ordnerfuersachenwoimsharedfoldernichtgehen\laich\m242\m242\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\fuse.h:
+
+c:\ordnerfuersachenwoimsharedfoldernichtgehen\laich\m242\m242\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\lock.h:
+
+C:\OrdnerFuerSachenWoImSharedFolderNichtGehen\laich\M242\m242\ToolChain\AvrLib\include/Atmega328P.h:
+
+C:\OrdnerFuerSachenWoImSharedFolderNichtGehen\laich\M242\m242\ToolChain\AvrLib\include/avrlib.h:
+
+c:\ordnerfuersachenwoimsharedfoldernichtgehen\laich\m242\m242\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\util\delay.h:
+
+c:\ordnerfuersachenwoimsharedfoldernichtgehen\laich\m242\m242\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\util\delay_basic.h:
+
+c:\ordnerfuersachenwoimsharedfoldernichtgehen\laich\m242\m242\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\math.h:
+
+c:\ordnerfuersachenwoimsharedfoldernichtgehen\laich\m242\m242\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\interrupt.h:
+
+c:\ordnerfuersachenwoimsharedfoldernichtgehen\laich\m242\m242\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\io.h:
--- HelloWorld/HelloWorld.c
+++ HelloWorld/HelloWorld.c
@@ -1,21 +1,20 @@
 #include <avr\io.h>
+#include <Atmega328P.h>
 #include <avrlib.h>
+#define F_CPU 16000000
 #include <util\delay.h>
-
 #include <inttypes.h>
+#include <avr/interrupt.h>
 
 int main(void)
 {
-	Usart_Init();
+	Usart_Init(250000);
 	
 	
-	uint8_t i = 10;
-	for (; i < 20; i++)
+	while (True)
 	{
-		TRACE("Hello World %8:", i);
-		TRACE("i times 2 I = %8, i*2=%16:", i, i*2);
-		TRACE("i square I = %8, i*2=%16:", i, i * i);
+		TRACE("Hello World");
 		_delay_ms(1000);
+		return 0;
 	}
-	return 0;
 }
